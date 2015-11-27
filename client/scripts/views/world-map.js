5c6fbf2e78c661235d6ba353e07f124e9903c425
Emilio <erix@jsjutsu.com>
2015-11-27 17:47:13 +0100

    Add map. Add npm amqplib and suscriptions file

---
+++ client/scripts/views/world-map.js
@@ -0,0 +1,73 @@
+Template.worldMap.onRendered(function() {
+  var self  = this;
+  this.map  = createMap();
+  this.attackLayers = [];
+
+  //addHoneypot(51.508, -0.11);
+  //addHoneypot(43, -87);
+
+  Streamy.on('attacks', function(d) {
+    paintAttacks(d.data);
+  });
+
+  function createMap() {
+    var map;
+    L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
+    map = L.map('worldMap',{
+      fullscreenControl: true
+    }).setView([40, -4], 2);
+    L.tileLayer.provider('CartoDB.Positron').addTo(map);
+    return map;
+  }
+
+  function addHoneypot (lat, lon) {
+    var customMarker = L.AwesomeMarkers.icon({
+      icon: 'empire',
+      markerColor: 'darkred',
+      prefix: 'fa'
+    });
+
+    L.marker([lat, lon], {icon: customMarker}).addTo(self.map);
+  }
+
+  function paintAttacks(attacks) {
+    // python send.py attacks '[["USA", 5000], ["ESP", 20], ["ITA", 500]]'
+    // Remove previous layers, if any
+    console.log(self.map);
+    if (typeof attackLayers != 'undefined') {
+      self.attackLayers.forEach(function(layer) {
+        map.removeLayer(layer);
+      });
+    }
+    self.attackLayers = [];
+
+    attacks.forEach(function(attack) {
+      $.getJSON('countries/' + attack[0] + '.geo.json', function(data) {
+        var layer = L.geoJson(data, {style: style(attack[1])});
+        self.attackLayers.push(layer);
+        layer.addTo(self.map);
+      });
+    });
+  }
+  function getColor(d) {
+      return d > 1000 ? '#800026' :
+             d > 500  ? '#BD0026' :
+             d > 200  ? '#E31A1C' :
+             d > 100  ? '#FC4E2A' :
+             d > 50   ? '#FD8D3C' :
+             d > 20   ? '#FEB24C' :
+             d > 10   ? '#FED976' :
+                        '#FFEDA0';
+  }
+
+  function style(density) {
+      return {
+          fillColor: getColor(density),
+          weight: 2,
+          opacity: 1,
+          color: 'white',
+          dashArray: '3',
+          fillOpacity: 0.7
+      };
+  }
+});
---
+++ server/event-subscriptions.js
@@ -0,0 +1,49 @@
+var open = Meteor.npmRequire('amqplib').connect('amqp://' + Meteor.settings.brokerHost);
+
+var susbscriptions = [
+  'attacks'
+];
+
+/**
+ * Open connection with AMQP Broker and subscribe to events.
+ */
+open
+.then(function(conn) {
+  return conn.createChannel();
+})
+.then(function(ch) {
+  Channel = ch;
+  return ch.assertExchange('hive', 'direct', {durable: false});
+})
+.then(function() {
+  Channel.assertQueue('', {exclusive: true})
+  .then(function(qok) {
+    susbscriptions.forEach(function(sub) {
+      Channel.bindQueue(qok.queue, 'hive', sub);
+    });
+    Channel.consume(qok.queue, callback, {noAck: true});
+  })
+});
+
+
+/**
+ * Event subscription callback.
+ * TODO: this should be a dispatcher for support many events.
+ */
+function callback(msg) {
+  // { fields:
+  //   { consumerTag: 'amq.ctag-1M11kkj9ijPJ7ZvAta7kAQ',
+  //     deliveryTag: 2,
+  //     redelivered: false,
+  //     exchange: 'hive',
+  //     routingKey: 'attacks' },
+  //  properties: {},
+  // content: <Buffer 68 6f 6c 61 61 61 61 61> }
+  var attacks;
+  try {
+    attacks = JSON.parse(msg.content.toString('utf8'));
+    Streamy.broadcast('attacks', { data: attacks });
+  } catch (err) {
+    console.log('Bad msg');
+  }
+}
