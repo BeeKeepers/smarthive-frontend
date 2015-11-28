36e619495743c897ac751d2b9782ad798204cd11
Emilio <erix@jsjutsu.com>
2015-11-28 21:48:09 +0100

    IP event working (fuck yeah!)

--- server/event-subscriptions.js
+++ server/event-subscriptions.js
@@ -1,4 +1,5 @@
 var open = Meteor.npmRequire('amqplib').connect('amqp://' + Meteor.settings.brokerHost);
+Fiber = Npm.require('fibers');
 
 /**
  * Open connection with AMQP Broker and subscribe to events.
@@ -36,13 +37,19 @@ function callback(msg) {
   //  properties: {},
   // content: <Buffer 68 6f 6c 61 61 61 61 61> }
   console.log('AMQP message! ', msg.content.toString('utf8'));
-  try {
+  var hp;
+  try { // TODO: move to real dispatch
     event = JSON.parse(msg.content.toString('utf8'));
     if (event.event_type === 'trend_attack') {
       Streamy.broadcast('attacks', { data: event.event_payload });
+    } else if (event.event_type === 'honeypot_ready') {
+      Fiber(function() {
+        var hp = InternetHoneypots.findOne({hostname: event.event_payload.hostname});
+        InternetHoneypots.update({_id: hp._id}, {$set:{ip: event.event_payload.ip}});
+      }).run();
     }
-
   } catch (err) {
+    console.log(err);
     console.log('Bad msg');
   }
 }
